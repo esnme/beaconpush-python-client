0f2be507ff4784d3834a5ab87a786bdb68158195
Carl Byström <cgbystrom@gmail.com>
2012-03-28 17:47:08 +0200

    Refactored event client.

--- beaconpush/eventclient.py
+++ beaconpush/eventclient.py
@@ -1,62 +1,89 @@
 # coding=UTF-8
-from beaconpush.socket import BeaconpushSocket
 import logging
 import gevent
 import gevent.pool
-logging = logging.getLogger("beaconpush.eventclient")
+import random
+from gevent import socket
 
+class Event(object):
+    def __init__(self, operator_id, name, user_id, channel):
+        self.operator_id = operator_id
+        self.name = name
+        self.user_id = user_id
+        self.channel = channel
 
-class EventReader(BeaconpushSocket):
-    def run(self, callback):
-        pool = gevent.pool.Pool(500)
-        i = 0
-        while True:
-            i += 1
-            if i % 100 == 0: logging.debug("Beaconpush Events: %d" % i)
-            try:
-                event = gevent.with_timeout(30, self.getResponse)
-            except gevent.Timeout:
-                event = True
-                continue
-
-            if event is None: # Something is wrong, reconnect
-                logging.warn("Event client got None, breaking out of read loop")
-                break
-            if pool.full():
-                logging.warn("Discarding event since the event pool is full")
-            else:
-                pool.spawn(lambda : callback(event))
+    def __repr__(self):
+        return "Event(operator_id=%s, name=%s, user_id=%s, channel=%s)" % (self.operator_id, self.name, self.user_id, self.channel)
 
 class EventClient():
     host = None
     port = 6051
 
-    def __init__(self, host, port=6051):
+    def __init__(self, host, event_handler, port=6051):
         self.host = host
         self.port = port
+        self.event_handler = event_handler
+        self.logger = logging.getLogger("beaconpush.eventclient.%s:%d" % (host, port))
+        self.connect_timeout = 5
+        self._event_receiver_task = None
+        self.events_received = gevent.queue.Queue()
+        gevent.spawn(self._event_dispatcher)
 
-    def connect(self, event_handler):
-        backoff = 1
+    def connect(self):
+        self.logger.debug("Connecting...")
+        try:
+            self.sock = socket.create_connection((self.host, self.port), timeout=self.connect_timeout)
+            self.logger.debug("Connected!")
+            self.backoff = 0
+
+            if not self._event_receiver_task:
+                self._event_receiver_task = gevent.spawn(self._event_receiver)
+        except socket.error as e:
+            self.logger.error("Unable to connect. Reason: '%s'" % e)
+            self.reconnect()
+
+    def disconnect(self):
+        self.logger.debug("Disconnecting...")
+        self.sock.close()
+        self.sock = None
+        self.logger.debug("Disconnected!")
+
+    def reconnect(self):
+        self.backoff += random.randint(3, 6) # Add some jitter to timeout to prevent thundering herd
+        self.backoff = min(self.backoff, 60)
+        self.logger.warn("Reconnecting in %d seconds..." % self.backoff)
+        gevent.sleep(self.backoff)
+        self.connect()
+
+    def _event_receiver(self):
+        self.logger.debug("Event receiver started.")
+
+        buffer = ""
         while True:
-            logging.info("Beaconpush Event Client connecting to %s:%d" % (self.host, self.port))
-            event_reader = EventReader(self.host, self.port)
-            try:
-                event_reader.connect()
-            except Exception:
-                logging.warn("Beaconpush Event Client failed to connect, make sure Beaconpush server is up and running at %s:%d" % (self.host, self.port))
-            else:
-                logging.info("Beaconpush Event Client connected to %s:%d" % (self.host, self.port))
-                backoff = 1 # Reset backoff upon successful connection
-                event_reader.run(event_handler)
+            # FIXME: What if we receive a read timeout?
+            data = self.sock.recv(1024)
+            if not data:
+                self.logger.warn("Unexpected disconnect.")
+                self.reconnect()
+                continue
 
-            # If we get here, the event client has failed/disconnected
-            try:
-                event_reader.disconnect()
-            except Exception:
-                logging.warn("Disconnected failed.")
+            buffer += data
+            events = buffer.split("\r\n") # Unframe events from buffer
+            buffer = events.pop() # Put back any incomplete event, often an empty string
 
-            logging.warn("Beaconpush Event Client disconnected. Waiting %s seconds before reconnecting" % (backoff, ))
-            gevent.sleep(backoff)
-            backoff += 5
-            if backoff > 60:
-                backoff = 60
+            for raw_event in events:
+                args = raw_event.split('\t')
+                if not len(args) == 4:
+                    self.logger.error("Received event has not 4 arguments. Event '%s'" % line)
+
+                e = Event(*args)
+                self.logger.debug("Received %s" % e)
+                self.events_received.put_nowait(e)
+
+        self.logger.debug("Event receiver finished.")
+
+    def _event_dispatcher(self):
+        # Do we really this? Can't we dispatch inside _event_receiver?
+        while True:
+            e = self.events_received.get()
+            self.event_handler(e)
